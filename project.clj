4cc22f92b9b372290044e6eb6091fd049c47ccb2
Naitik Shah <n@daaku.org>
2012-03-10 16:01:07 -0800

    remove unused urly dependency

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "URI, URL, query strings and so on."
   :author "Naitik Shah <n@daaku.org>"
   :url "https://github.com/nshah/duri.clj"
-  :dependencies [[clojurewerkz/urly "1.0.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]])
